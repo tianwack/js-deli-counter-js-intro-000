a75c8fd7c33a9e004ac50244f92447dbce590db9
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 19:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(`${name}`)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+function takeANumber(katzDeli, name){
+  katzDeli.push(`${name}`)
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
 }
 
-function nowServing(katzDeliLine){
+function nowServing(katzDeli){
   if (katzDeliLine.length > 0) {
     var nowServing = katzDeli[0];
     katzDeli.shift();
