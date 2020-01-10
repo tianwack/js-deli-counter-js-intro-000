5d42386402f2d335798e88a1c2bae9a8da776519
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 19:30:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(`${name}`)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  let i = 0
+  while i < (katzDeliLine.length){
+    i++;
+  }
+  if (katzDeliLine.length === 0){
+  return "There is nobody waiting to be served!"
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`)
+}
