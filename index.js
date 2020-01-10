e4ea28ef5a793fa6aa86a8e7fa643c1c75fd31cb
Christian Wacker <christianwwacker@gmail.com>
2020-01-10 19:47:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function nowServing(katzDeli){
 
 function currentLine(line){
   if(line.length === 0){
-    return `The line is curently empty.`;
+    return `The line is currently empty.`;
   } else {
 
     var array = []
     for (var i = 0; i < line.length; i++){
-      array.push(`${i + 1}. ${line[i]}`)
+      array.push(`${i + 1}. ${line[i]} `)
     }
     return `The line is currently: ` + array;
   }
